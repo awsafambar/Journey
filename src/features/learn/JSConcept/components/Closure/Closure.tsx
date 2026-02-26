0ac6e59fed8779b176f290c147607886e568701f
awsafambar <awsafamber@gmail.com>
2026-02-26 19:01:33 +0530

    xx

--- src/features/learn/JSConcept/components/Closure/Closure.tsx
+++ src/features/learn/JSConcept/components/Closure/Closure.tsx
@@ -5,7 +5,7 @@ export const Closure = () => {
         <>
             <div>{"Functions bundled together in its lexical environment ."}</div>
             <div>{"In JS, closures are created everytime a function is created at, at function creation time "}</div>
-            <div>{"It gives access to another scope variable from inner function even after it is executed or closed."}</div>
+            <div>{"It gives access to another scope variable from inner function even after it is executed or closed. and not garbage collected"}</div>
         </>
     )
 }
